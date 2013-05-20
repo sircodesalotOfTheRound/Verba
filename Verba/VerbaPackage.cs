17fc9ceb4349430e8cf266d7816f43d78cb4af7d
Sircodesalot <sircodesalot@gmail.com>
2013-05-20 14:21:25 -0700

    Verba Project Files
    Project Files

---
+++ Verba/VerbaPackage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+
+
+namespace Verba
+{
+    [Guid(GuidList.guidVerbaPkgString)]
+    public class VerbaPackage : VerbaPackageBase
+    {
+    }
+}
