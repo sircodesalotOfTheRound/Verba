086a37ffd70bfb9c4e450d31b4676bf4b03ca4be
Reuben Kuhnert <sircodesalot@gmail.com>
2014-12-05 09:06:17 -0600

    #40 Organize keywords

--- src/main/java/com/verba/language/parse/tokens/identifiers/KeywordToken.java
+++ src/main/java/com/verba/language/parse/tokens/identifiers/KeywordToken.java
@@ -16,35 +16,76 @@ public class KeywordToken extends IdentifierToken {
       "char", "utc", "currency",
       "utf", "utf8", "utf16", "utf32", "utf8be", "utf16be", "utf32be",
       "unit", "object",
-      "json", "sensitive");
+      "json");
+
+  private static final QSet<String> accessModifiers = new QSet<String>(
+    "public", "private", "protected", "internal", "static"
+  );
+
+  private static final QSet<String> typeDeclaration = new QSet<String>(
+    "val", "fn", "class", "trait", "sql", "extend", "namespace",
+    "meta", "template", "markup", "proxy", "signature", "segment",
+    "options", "option"
+  );
+
+  private static final QSet<String> declarationModifiers = new QSet<String>(
+    "abstract", "mut", "native", "sensitive"
+  );
+
+  private static final QSet<String> functionModifiers = new QSet<String>(
+    "sync", "async", "operator", "virtual", "override", "abstract", "fork"
+  );
+
+  private static final QSet<String> objectInstantiation = new QSet<String>(
+    "new", "inject", "resolve"
+  );
+
+  private static final QSet<String> operators = new QSet<String>(
+    "to", "in", "yield"
+  );
+
+  private static final QSet<String> reflection = new QSet<String>(
+    "is", "isa", "hasa", "some", "none", "def"
+  );
+
+  private static final QSet<String> control = new QSet<String>(
+    "if", "else", "assuming", "unless", "for", "while",
+    "do", "break", "continue", "match", "case", "return"
+  );
+
+  private static final QSet<String> booleans = new QSet<String>(
+    "true", "false"
+  );
+
+  private static final QSet<String> polymorphishm = new QSet<String>(
+    "this", "base"
+  );
+
+  private static final QSet<String> exceptions = new QSet<String>(
+    "throw", "try", "catch", "finally"
+  );
 
   private static final QSet<String> otherKeywords = new QSet<String>(
-      "public", "private", "protected", "internal",
-      "static", "operator", "override", "segment", "virtual", "injected",
-      "to", "in", "withns", "trait", "interface",
-      "options", "option", "get", "set", "inline",
-      "namespace", "class", "abstract", "extend", "sql", "native",
-      "signature", "fn", "meta", "template", "fork", "proxy",
-      "val", "mut", "yield",
-      "new", "inject",
-      "return",
-      "if", "then", "else", "unless", "assuming",
-      "until", "for", "while", "do", "break", "continue",
-      "isa", "hasa",
-      "throw", "try", "catch", "finally",
-      "this", "base",
-      "none", "some", "is",
-      "def", "resolve",
-      "match", "case",
-      "true", "false",
-      "sync", "async", "threadlocal");
+      "withns", "threadlocal"
+  );
 
   private static QSet<String> keywords = new Supplier<QSet<String>>() {
     @Override
     public QSet<String> get() {
       QSet<String> allKeywords = new QSet<String>();
       allKeywords.add(nativeTypes);
+      allKeywords.add(accessModifiers);
       allKeywords.add(otherKeywords);
+      allKeywords.add(typeDeclaration);
+      allKeywords.add(declarationModifiers);
+      allKeywords.add(functionModifiers);
+      allKeywords.add(objectInstantiation);
+      allKeywords.add(operators);
+      allKeywords.add(reflection);
+      allKeywords.add(control);
+      allKeywords.add(booleans);
+      allKeywords.add(polymorphishm);
+      allKeywords.add(exceptions);
 
       return allKeywords;
     }
@@ -62,6 +103,10 @@ public class KeywordToken extends IdentifierToken {
 
   public static boolean isKeyword(String text) {
     return KeywordToken.keywords.contains(text);
+  }
+
+  public static boolean isAccessModifierKeyword(String fqn) {
+    return KeywordToken.accessModifiers.contains(fqn);
   }
 
   public static boolean isNativeTypeKeyword(String fqn) {
