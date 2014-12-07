e6beea67eb3fc89670d391a404fa0a28e75d2303
Reuben Kuhnert <sircodesalot@gmail.com>
2014-12-07 13:53:14 -0600

    #63 Clean up SymbolTableExpression

--- src/main/java/com/verba/language/parse/expressions/categories/SymbolTableExpression.java
+++ src/main/java/com/verba/language/parse/expressions/categories/SymbolTableExpression.java
@@ -1,10 +1,11 @@
 package com.verba.language.parse.expressions.categories;
 
 import com.verba.language.graph.symbols.table.tables.Scope;
+import com.verba.language.graph.visitors.ExpressionTreeNode;
 
 /**
  * An expression that can contain a symbol table
  */
-public interface SymbolTableExpression extends ExpressionCategory {
+public interface SymbolTableExpression extends ExpressionTreeNode, ExpressionCategory {
   void accept(Scope symbolTable);
 }
