9cf8bc1a727b0552d7f1059d777fb7547dd4f62f
Reuben Kuhnert <sircodesalot@gmail.com>
2014-12-08 08:19:22 -0600

    #68 - Make parent search more targeted.

--- src/main/java/com/verba/language/parse/expressions/backtracking/rules/FunctionDeclarationBacktrackRule.java
+++ src/main/java/com/verba/language/parse/expressions/backtracking/rules/FunctionDeclarationBacktrackRule.java
@@ -2,8 +2,10 @@ package com.verba.language.parse.expressions.backtracking.rules;
 
 import com.verba.language.parse.expressions.VerbaExpression;
 import com.verba.language.parse.expressions.backtracking.BacktrackRule;
+import com.verba.language.parse.expressions.block.BlockDeclarationExpression;
 import com.verba.language.parse.expressions.blockheader.classes.PolymorphicDeclarationExpression;
 import com.verba.language.parse.expressions.blockheader.functions.FunctionDeclarationExpression;
+import com.verba.language.parse.expressions.modifiers.DeclarationModifierExrpression;
 import com.verba.language.parse.info.LexList;
 import com.verba.language.parse.lexing.Lexer;
 import com.verba.language.parse.tokens.identifiers.KeywordToken;
@@ -39,14 +41,17 @@ public class FunctionDeclarationBacktrackRule extends BacktrackRule {
       return null;
     }
 
-    // If the 'immediate-ish' parent is a class, then return that class.
-    // If the parent is a function, then return nothing (can't be a construct.
+    // If the parent is a class declaration, then return that.
+    // If the parent is a modifier (public/private/...) or block, then check one level up.
+    // Otherwise, return null - the parent isn't a class.
     if (expression.is(PolymorphicDeclarationExpression.class)) {
       return (PolymorphicDeclarationExpression) expression;
-    } else if (expression.is(FunctionDeclarationExpression.class)) {
-      return null;
+    } else if (expression.is(DeclarationModifierExrpression.class)) {
+      return determineIfPartOfClass(expression.parent());
+    } else if (expression.is(BlockDeclarationExpression.class)) {
+      return determineIfPartOfClass(expression.parent());
     } else {
-        return determineIfPartOfClass(expression.parent());
+      return null;
     }
   }
 }
