cfc373a32767e826cb583c746aabf8bc76ac48ab
Reuben Kuhnert <reuben.kuhnert@cloudera.com>
2015-03-16 18:51:27 -0500

    #170 Move IterableMap to javaLinq (QMap)

--- src/main/java/com/verba/language/build/targets/artifacts/interfaces/BuildArtifactSet.java
+++ src/main/java/com/verba/language/build/targets/artifacts/interfaces/BuildArtifactSet.java
@@ -1,14 +1,14 @@
 package com.verba.language.build.targets.artifacts.interfaces;
 
 import com.javalinq.interfaces.QIterable;
-import com.verba.tools.datastructures.iterablemap.IterableMap;
-import com.verba.tools.datastructures.iterablemap.KeyValuePair;
+import com.javalinq.implementations.QMap;
+import com.javalinq.tools.KeyValuePair;
 
 /**
  * Created by sircodesalot on 15/3/12.
  */
 public class BuildArtifactSet implements BuildArtifactContainer {
-  private final IterableMap<Class, BuildArtifact> artifacts = new IterableMap<>();
+  private final QMap<Class, BuildArtifact> artifacts = new QMap<>();
 
   public <T extends BuildArtifact> boolean containsArtifactOfType(Class<T> type) {
     return artifacts.containsKey(type);
