21580829b5272643de408068d882aa3473d6948e
Reuben Kuhnert <reuben.kuhnert@cloudera.com>
2015-03-18 10:30:39 -0500

    #187 Add BuildTargetDependencyGraph

--- src/main/java/com/verba/language/build/events/BuildTargetDependencyGraph.java
+++ src/main/java/com/verba/language/build/events/BuildTargetDependencyGraph.java
@@ -3,19 +3,32 @@ package com.verba.language.build.events;
 import com.javalinq.implementations.QList;
 import com.javalinq.implementations.QMap;
 import com.javalinq.interfaces.QIterable;
+import com.verba.language.build.targets.artifacts.interfaces.BuildArtifact;
 import com.verba.language.build.targets.interfaces.BuildTarget;
+import com.verba.testtools.datastructures.OneToManyMap;
 import com.verba.testtools.polymorphism.ClassHierarchyFlattener;
 
 /**
  * Created by sircodesalot on 15/3/18.
  */
 public class BuildTargetDependencyGraph {
-  private final QMap<Class, QList<BuildTarget>> targetMap = new QMap<>();
+  private final OneToManyMap<Class, BuildTarget> targetMap = new OneToManyMap<>();
 
   public void addTarget(BuildTarget target) {
     QIterable<Class> allDependencies = target.dependencies()
       .map(ClassHierarchyFlattener::new)
       .flatten(ClassHierarchyFlattener::flattenedHierarchy);
 
+    for (Class dependency : allDependencies) {
+      targetMap.add(dependency, target);
+    }
+  }
+
+  public boolean containsTargetsForDependency(BuildArtifact artifact) {
+    return targetMap.containsKey(artifact.getClass());
+  }
+
+  public QIterable<BuildTarget> getTargetsForDependency(BuildArtifact artifact) {
+    return targetMap.getItemsForKey(artifact.getClass());
   }
 }
